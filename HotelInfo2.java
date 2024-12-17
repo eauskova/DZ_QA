23a66c093f26e50ca3e1733bb2833c23f6105df4
eauskova <124862140+eauskova@users.noreply.github.com>
2024-12-17 18:28:24 +0300

    Create HotelInfo2.java

---
+++ HotelInfo2.java
@@ -0,0 +1,50 @@
+package com.example.pages;
+
+public class HotelInfo {
+    private String name;
+    private int stars;
+    private String averageScore;
+    private String reviewsCount;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public String getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(String averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public String getReviewsCount() {
+        return reviewsCount;
+    }
+
+    public void setReviewsCount(String reviewsCount) {
+        this.reviewsCount = reviewsCount;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
+ 
